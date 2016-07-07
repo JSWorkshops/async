e794e65fac015543aae0826a66587d7716ed95f6
Marcos Caceres <marcos@marcosc.com>
2016-07-07 16:53:54 +1000

    Feat(async): add async.js + test spec

--- tutorial/async.js
+++ tutorial/async.js
@@ -1,59 +1,39 @@
-/*jshint node:true, browser:true, noyield:true, esnext: true */
+/*jshint  -W082 */
 /*globals define*/
-
-function*(){
-  try{
-    try{
-      yield Promise.reject(new Error())
-    }catch(err){
-      throw err;
-    }
-  }
-}
-
-"use strict";
+'use strict';
 {
   // async function takes a generator, and optional "this"
   function async(func, self) {
-    // returns returns a function asyncFunction that returns promise
+    // 1. Throws if not a function
+
+    // returns returns a function "asyncFunction" that returns promise
     // It is called with zero or more arguments...
     return function asyncFunction(...args) {
       return new Promise((resolve, reject) => {
-        if(typeof func !== "function"){
-          throw new TypeError("need a function!");
-        }
-        const gen = func.call(self, ...args);
-        if(gen.constructor.name !== "GeneratorFunction"){
-          return resolve(gen);
-        }
+        let gen;
+        // 2. func is not a generator, then wrap it.
 
-        try {
-          step(gen.next());
-        }catch(err){
-          reject(err);
-        }
+        // 3. step the generator
 
-        function step({value, done}){
-          if(done){
-            return resolve(value);
-          }
-          if(!isThenable(value)){
-            return step(gen.next(value));
-          }
-          // promise ? jQuery? blue
-          Promise
-            .resolve(value)
-            .then(
-              value => step(gen.next(value)),
-              err => step(gen.throw(err))
-            )
-            .catch(reject);
+        function step({value, done}) {
+          // 4. if we are done, resolve.
+
+          // 5. normalize promise
+
+          // 6. then(handle, resolve, error)
+
+          // 7. catch any potential errors
         }
       });
     };
   }
 
-  function isThenable(obj) {
-    return obj instanceof Promise || (typeof obj === "object" && typeof obj.then === "function");
+  // 8. Export to node, browser, etc. ("browserify")
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = async;
+  } else if (typeof define === 'function' && define.amd) {
+    define([], () => async);
+  } else {
+    window.async = async;
   }
 }
